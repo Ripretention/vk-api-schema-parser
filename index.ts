d70bd5b9a87a4622b6d789fa6ec5fb12fb98242c
Ripretention <r5r44e@gmail.com>
2022-07-19 17:14:15 +0300

    Add Schema Downloader, refactor output log

--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
 import { Generator } from "./src/Generator";
 import { ObjectSchemaParser } from "./src/parsers/ObjectSchemaParser";
+import { SchemaDownloader } from "./src/SchemaDownloader";
 
+const schemaDownloader = new SchemaDownloader("objects");
 const generator = new Generator();
 (async () => {
-	console.log("creating objects schema...");
+	console.log("⇩ downloading raw objects schema...");
+	await schemaDownloader.download("objects.json");
+	console.log("✔ object schema has been downloaded");
+	console.log("⟳ generating objects schema...");
 	await generator.generate(
 		"Objects.ts", 
 		require("../objects.json"), 
 		new ObjectSchemaParser()
 	);
 
-	console.log("schemas has been created");
+	console.log("✔ schemas have been created");
 })().catch(e => {
 	throw e;
 });
---
+++ src/SchemaDownloader.ts
@@ -0,0 +1,37 @@
+import { Agent, get, RequestOptions } from "https";
+import { createWriteStream } from "fs";
+
+export class SchemaDownloader {
+	constructor(private readonly schemaTitle: string) {}
+
+	public async download(
+		output = this.schemaTitle + ".ts",
+		threadLabel = "master",
+		optinos?: RequestOptions
+	) {
+		let outputStream = createWriteStream(output);
+
+		return new Promise((resolve, reject) => {
+			let req = get({
+				hostname: "raw.githubusercontent.com",
+				path: `VKCOM/vk-api-schema/${threadLabel}/${this.schemaTitle}.json`,
+				agent: new Agent({ keepAlive: true }),
+				...(optinos ?? {})
+			}, res => {
+				res.pipe(outputStream);
+				res.on("end", () => {
+					outputStream.close();
+					resolve(undefined);
+				});
+			});
+
+			req.on("error", err => {
+				outputStream.destroy();
+				err.message = `Caught until downloading ${this.schemaTitle}.json to ${output}, ${err.message}`;
+				reject(err);
+			});
+
+			req.end();
+		});
+	}
+}
